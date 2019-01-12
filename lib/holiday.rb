f0e3f99d19226da9df4e12a7b0a3cc5482f2b948
Lemrabt Boudadya <lboudadya@gmail.com>
2019-01-12 22:32:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,7 +85,7 @@ def all_holidays_with_bbq(holiday_hash)
 holiday_hash.collect do |season, holidays|
   holidays.collect do |holiday, items|
     if items.include?("BBQ")
-        holiday.flatten
+        holiday
       end
 end
 end 
